f31dc1aae15a3455b0300409dc4849c8850f4b5d
jadebuffy <jade.buffy@hotmail.fr>
2023-01-17 13:35:16 +0100

    Debug de la nav

--- header.php
+++ header.php
@@ -4,20 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <?php wp_head(); ?>
 </head>
 <body>
     <header class="head">
-        <nav role="navigation">
-            <ul class="nav-menu">
-                <li><img src="<?php echo(get_template_directory_uri())?>/assets/images/logo.svg"/></li>
-                <a href="http://localhost/wordpress/home/"><li>Cours de Yoga</li></a>
-                <a href="http://localhost/wordpress/a-propos/"><li>À propos</li></a>
-                <a href="http://localhost/wordpress/galerie/"><li>Galerie</li></a>
-                <a href="http://localhost/wordpress/creations/"><li>Mes créations</li></a>
-                <a href="http://localhost/wordpress/contact/"><li>Contact</li></a>
-            </ul>
+        <nav role="navigation" class="nav-menu">
+            <?php wp_nav_menu(array('theme_location' => 'primary_menu')); ?>
         </nav>
         <div class="titre">
             <h1>Yoga<br><p>Terre-Ciel</p></h1>
