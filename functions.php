2dee5cbd8ccf73d56d97d19877763890c0e8e28e
jadebuffy <jade.buffy@hotmail.fr>
2023-01-17 13:33:10 +0100

    menu wordpress

--- functions.php
+++ functions.php
@@ -7,4 +7,11 @@
       add_action( 'wp_enqueue_scripts', 'add_style' );
     // wp_enqueue_style('app', get_template_directory_uri().'', array(), 1, 'all'); 
 
+
+    function custom_register_nav_menu(){
+      register_nav_menus( array(
+          'primary_menu' => 'Menu principal',
+      ) );
+    }
+   add_action( 'after_setup_theme', 'custom_register_nav_menu', 0 );
 ?>
