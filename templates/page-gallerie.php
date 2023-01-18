9c7db2b06a911e41d8f207206ac102532e7fa2f3
jadebuffy <jade.buffy@hotmail.fr>
2023-01-18 10:15:24 +0100

    création de la gallerie

--- templates/page-gallerie.php
+++ templates/page-gallerie.php
@@ -18,14 +18,41 @@
             <h3>Photos</h3>
             <i class="icone-photo"></i>
         </div>
-        <!--GALERIE-->
+        <!--GALERIE PHOTO-->
+        <div class="gallerie">
+            <?php 
+                $images = get_field('galerie');
+                if( $images ): ?>
+                    <ul class="acf-gallery">
+                        <?php foreach( $images as $image ): ?>
+                            <li>
+                                <a href="<?php echo esc_url($image['url']); ?>">
+                                    <img src="<?php echo esc_url($image['sizes']['gallery-thumb']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                                </a>
+                                <p><?php echo esc_html($image['caption']); ?></p>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+        </div>
     </article>
     <article>
         <div class="t">
             <h3>Vidéos</h3>
             <i class="icone-video"></i>
         </div>
-        <!--GALERIE-->
+        <!--GALERIE VIDEO-->
+        <?php if( have_rows( 'gallerie_video' ) ): ?>
+        <div class="gallerie_video">
+            <?php while ( have_rows( 'gallerie_video' ) ) : the_row(); ?>
+            <div class="videos">
+                <video controls width=100%>
+                    <source src="<?php the_field("videos");?>" type="video/mp4">
+                </video>
+            </div>
+            <?php endwhile; ?>
+        </div>
+        <?php endif; ?>
     </article>
     <img src="../wp-content/themes/yogaterreciel/assets/images/footerGallerie.png" class="imgfoot">
 </section>   
