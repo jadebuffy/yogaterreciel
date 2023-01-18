48d5010aa18a74dbf09c6ca22cedf29d402ccb66
jadebuffy <jade.buffy@hotmail.fr>
2023-01-18 10:16:02 +0100

    update changement des liens (administrabilité)

--- templates/page-jewerlies.php
+++ templates/page-jewerlies.php
@@ -20,20 +20,18 @@
     </article>
     <article div="cours">
         <div class="video">
-            <iframe width="684" height="385" src="https://www.youtube.com/embed/52nfjRzIaj8?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbAVMaGSKrC7dGcY" title="I'll Never Love Again (from A Star Is Born) (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <video controls width=100%>
+                <source src="<?php the_field("video_med"); ?>" type="video/mp4">
+            </video>
         </div>
         <div class="desc">
             <div class="t">
-                <h3>Méditation guidée</h3>
+                <h3><?php the_field("title"); ?></h3>
                 <i class="icone-meditation"></i>
             </div>
             <div class="paragraphe">
-                <p>La méditation guidée est un outil d'apprentissage de la méditation, 
-                        elle permet de s'initier à cette discipline  tout en en étant guidé. 
-                        Je vous propose dans cette dernière de vous initier à cette discipline  
-                        avec des techniques simples et accessibles.</p>
-                <p>Vous êtes intéressés par une séance de méditation privée? Laissez-moi vos coordonnées</p>
-                <a href=""><button>Je suis intéressé.e</button></a>
+                <p><?php the_field("description"); ?></p>
+                <a href=""><button class="bouton">Je suis intéressé.e</button></a>
             </div>
         </div>
     </article>
@@ -41,8 +39,7 @@
 <section class="page-jewerlies2">
     <h2>Les bijoux</h2>
     <article>
-        <p>Bien que je sois professeure de yoga, je suis aussi créatrice de bijoux. 
-        Ici vous pourrez retrouver mes dernières créations : bagues, bracelets, colliers,...</p>
+        <p><?php the_field("texte_bijoux"); ?></p>
         <!--SLIDER-->
     </article>
     <img src="../wp-content/themes/yogaterreciel/assets/images/footerJewerlies.png" class="imgfoot"/>
