4c32cd375e3e556efb3695ca933748cd3ed7750f
jadebuffy <jade.buffy@hotmail.fr>
2023-01-18 10:15:14 +0100

    update des images header qui changent à chaque page

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo(get_the_title()); ?></title>
     <?php wp_head(); ?>
 </head>
 <body>
@@ -11,8 +12,14 @@
         // Image
         $image = get_field('imagefond');
     ?>
-    <header class="head" style="background-image:url(<?php echo($image['url']); ?>);">
+    <header class="head" style="background-image:url(<?php echo($image['url']); ?>); width: 100%; 
+    height: 810px;
+    box-sizing: border-box;
+    background-repeat: no-repeat;
+	background-size: cover; 
+	background-position: center;">
         <nav role="navigation" class="nav-menu">
+            <img src="../wp-content/themes/yogaterreciel/assets/images/logo.svg"/>
             <?php wp_nav_menu(array('theme_location' => 'primary_menu')); ?>
         </nav>
         <div class="titre">
